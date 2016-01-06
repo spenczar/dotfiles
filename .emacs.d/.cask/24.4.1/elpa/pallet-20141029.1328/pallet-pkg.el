06b3daac9600383d4cf2e8d28bdf58d1067fd2ca
Spencer Nelson <spencer@twitch.tv>
2016-01-06 11:52:45 -0500

    Fix solarized on OSX

--- .emacs.d/.cask/24.4.1/elpa/pallet-20141029.1328/pallet-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "pallet" "20141029.1328" "A package management tool for Emacs, using Cask."
-  '((cask "0.6")))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,7 +1,36 @@
-(require 'cask "/usr/local/Cellar/cask/0.7.2/cask.el")
-(cask-initialize)
-(require 'pallet)
-(pallet-mode t)
+(if (string-equal system-type "darwin")
+    (progn
+      (message "Running on OSX")
+      (require 'cask "/usr/local/Cellar/cask/0.7.2/cask.el")
+      (cask-initialize)
+      (require 'pallet)
+      (pallet-mode t)
+
+      ;; Color Theme
+      (require 'color-theme)
+      (load-theme 'solarized t)
+
+      ;; copying and pasting to main clipboard
+      (defun pbcopy ()
+	(interactive)
+	(call-process-region (point) (mark) "pbcopy")
+	(setq deactivate-mark t))
+
+      (defun pbpaste ()
+	(interactive)
+	(call-process-region (point) (if mark-active (mark) (point)) "pbpaste" t t))
+
+      (defun pbcut ()
+	(interactive)
+	(pbcopy)
+	(delete-region (region-beginning) (region-end)))
+
+      (global-set-key (kbd "S-c") 'pbcopy)
+      (global-set-key (kbd "S-v") 'pbpaste)
+      (global-set-key (kbd "S-x") 'pbcut)
+      )
+  (message (concat "Running on " system-type))
+  )
 
 (require 'windmove)
 (global-set-key (kbd "s-J")  'windmove-left)
@@ -36,42 +65,12 @@
  kept-old-versions 2
     version-control t)       ; use versioned backups
 
-;; copying and pasting to main clipboard
-(defun pbcopy ()
-  (interactive)
-  (call-process-region (point) (mark) "pbcopy")
-  (setq deactivate-mark t))
-
-(defun pbpaste ()
-  (interactive)
-  (call-process-region (point) (if mark-active (mark) (point)) "pbpaste" t t))
-
-(defun pbcut ()
-  (interactive)
-  (pbcopy)
-  (delete-region (region-beginning) (region-end)))
-
-(global-set-key (kbd "S-c") 'pbcopy)
-(global-set-key (kbd "S-v") 'pbpaste)
-(global-set-key (kbd "S-x") 'pbcut)
-
 ;;; No sleeping!
 (global-unset-key (kbd "C-z"))
 
 ;;; Hide ~ files in directories
 (require 'dired-x)
 (setq-default dired-omit-files-p t) ; Buffer-local variable
-
-;;; Color Theme
-(require 'color-theme)
-(if (daemonp)
-    (add-hook 'after-make-frame-functions
-              (lambda (frame)
-                (select-frame frame)
-                (load-theme 'solarized-dark t)))
-  (load-theme 'solarized-dark t))
-(color-theme-initialize)
-(color-theme-solarized-dark)
 
 ;;; Column indication
 ;; make column number mode the default
@@ -96,28 +95,102 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#073642" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#657b83"])
  '(asm-comment-char 47)
+ '(compilation-message-face (quote default))
+ '(cua-global-mark-cursor-color "#2aa198")
+ '(cua-normal-cursor-color "#839496")
+ '(cua-overwrite-cursor-color "#b58900")
+ '(cua-read-only-cursor-color "#859900")
+ '(custom-safe-themes
+   (quote
+    ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" default)))
  '(dired-listing-switches "-alh")
  '(dired-omit-files "^#\\|^\\.$\\$")
+ '(fci-rule-color "#073642")
  '(fill-column 79)
- '(flycheck-eslintrc "(substitute-in-file-name \"$HOME/.eslintrc\")")
+ '(flycheck-eslintrc (substitute-in-file-name "$HOME/.eslintrc"))
+ '(frame-background-mode (quote dark))
  '(help-at-pt-display-when-idle (quote (flymake-overlay)) nil (help-at-pt))
  '(help-at-pt-timer-delay 0.0)
  '(help-at-pt-timer-display 0.9)
+ '(highlight-changes-colors (quote ("#d33682" "#6c71c4")))
+ '(highlight-symbol-colors
+   (--map
+    (solarized-color-blend it "#002b36" 0.25)
+    (quote
+     ("#b58900" "#2aa198" "#dc322f" "#6c71c4" "#859900" "#cb4b16" "#268bd2"))))
+ '(highlight-symbol-foreground-color "#93a1a1")
+ '(highlight-tail-colors
+   (quote
+    (("#073642" . 0)
+     ("#546E00" . 20)
+     ("#00736F" . 30)
+     ("#00629D" . 50)
+     ("#7B6000" . 60)
+     ("#8B2C02" . 70)
+     ("#93115C" . 85)
+     ("#073642" . 100))))
+ '(hl-bg-colors
+   (quote
+    ("#7B6000" "#8B2C02" "#990A1B" "#93115C" "#3F4D91" "#00629D" "#00736F" "#546E00")))
+ '(hl-fg-colors
+   (quote
+    ("#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36")))
  '(js-indent-level 2)
  '(js2-basic-offset 2)
  '(js2-global-externs (quote ("require")))
  '(js2-highlight-level 3)
+ '(magit-diff-use-overlays nil)
+ '(nrepl-message-colors
+   (quote
+    ("#dc322f" "#cb4b16" "#b58900" "#546E00" "#B4C342" "#00629D" "#2aa198" "#d33682" "#6c71c4")))
  '(package-archives
    (quote
     (("melpa" . "http://melpa.milkbox.net/packages/")
-     ("elpa" . "http://elpa.gnu.org/packages/"))))
+     ("elpa" . "http://elpa.gnu.org/packages/")
+     ("marmalade" . "http://marmalade-repo.org/packages/"))))
+ '(pos-tip-background-color "#073642")
+ '(pos-tip-foreground-color "#93a1a1")
  '(puppet-include-indent 2)
  '(require-final-newline t)
+ '(smartrep-mode-line-active-bg (solarized-color-blend "#859900" "#073642" 0.2))
+ '(term-default-bg-color "#002b36")
+ '(term-default-fg-color "#839496")
  '(terraform-indent-level 2)
  '(tidy-shell-command "/usr/local/bin/tidy --tidy-mark false -indent")
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#dc322f")
+     (40 . "#ff7f00")
+     (60 . "#ffbf00")
+     (80 . "#b58900")
+     (100 . "#ffff00")
+     (120 . "#ffff00")
+     (140 . "#ffff00")
+     (160 . "#ffff00")
+     (180 . "#859900")
+     (200 . "#aaff55")
+     (220 . "#7fff7f")
+     (240 . "#55ffaa")
+     (260 . "#2affd4")
+     (280 . "#2aa198")
+     (300 . "#00ffff")
+     (320 . "#00ffff")
+     (340 . "#00ffff")
+     (360 . "#268bd2"))))
+ '(vc-annotate-very-old-color nil)
  '(web-mode-code-indent-offset 2)
- '(web-mode-markup-indent-offset 2))
+ '(web-mode-markup-indent-offset 2)
+ '(weechat-color-list
+   (quote
+    (unspecified "#002b36" "#073642" "#990A1B" "#dc322f" "#546E00" "#859900" "#7B6000" "#b58900" "#00629D" "#268bd2" "#93115C" "#d33682" "#00736F" "#2aa198" "#839496" "#657b83")))
+ '(xterm-color-names
+   ["#073642" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#eee8d5"])
+ '(xterm-color-names-bright
+   ["#002b36" "#cb4b16" "#586e75" "#657b83" "#839496" "#6c71c4" "#93a1a1" "#fdf6e3"]))
 ;;(add-hook 'find-file-hook 'flymake-find-file-hook)
 
 ;;; Electric Pairs
