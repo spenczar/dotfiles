022c5836db7f5bd11c5b3901530b70e638dd2451
Spencer Nelson <spencer@justin.tv>
2016-01-25 12:29:56 -0500

    Remove mouse package

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -58,26 +58,6 @@
   )
 
 ;;; Navigation
-;; Enable mouse support if in a terminal
-(unless window-system
-  (use-package mouse
-    :config
-    (global-set-key [mouse-4] '(lambda ()
-				 (interactive)
-				 (scroll-down 1)))
-    (global-set-key [mouse-5] '(lambda ()
-				 (interactive)
-				 (scroll-up 1)))
-    (xterm-mouse-mode t)
-    (defun track-mouse (e))
-    (setq mouse-sel-mode t)))
-
-;; Project navigation
-(use-package projectile
-  :ensure t
-  :config
-  (projectile-global-mode))
-
 
 ;; Manage backup files
 (setq backup-directory-alist
